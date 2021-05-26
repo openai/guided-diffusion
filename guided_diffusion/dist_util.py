0ba878e517b276c45d1195eb29f6f5f72659a05b
Alex Nichol <unixpickle@gmail.com>
2021-05-26 15:15:14 -0400

    fix cuda memory, super res settings, fp16 training (#5)

--- guided_diffusion/dist_util.py
+++ guided_diffusion/dist_util.py
@@ -24,6 +24,7 @@ def setup_dist():
     """
     if dist.is_initialized():
         return
+    os.environ["CUDA_VISIBLE_DEVICES"] = f"{MPI.COMM_WORLD.Get_rank() % GPUS_PER_NODE}"
 
     comm = MPI.COMM_WORLD
     backend = "gloo" if not th.cuda.is_available() else "nccl"
@@ -46,7 +47,7 @@ def dev():
     Get the device to use for torch.distributed.
     """
     if th.cuda.is_available():
-        return th.device(f"cuda:{MPI.COMM_WORLD.Get_rank() % GPUS_PER_NODE}")
+        return th.device(f"cuda")
     return th.device("cpu")
 
 

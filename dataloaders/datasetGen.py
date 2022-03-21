Added task conditional generations + fix issue with image scaling

dataloaders/datasetGen.py
@@ -7,73 +7,7 @@ from torch.utils.data import Subset
 from .wrapper import Subclass, AppendName, Permutation
 
 
-def SplitGen(train_dataset, val_dataset, first_split_sz=2, other_split_sz=2, random_split=False, remap_class=True):
-    '''
-    Generate the dataset splits based on the labels.
-    :param train_dataset: (torch.utils.data.dataset)
-    :param val_dataset: (torch.utils.data.dataset)
-    :param first_split_sz: (int)
-    :param other_split_sz: (int)
-    :param rand_split: (bool) Randomize the set of label in each split
-    :param remap_class: (bool) Ex: remap classes in a split from [2,4,6 ...] to [0,1,2 ...]
-    :return: train_loaders {task_name:loader}, val_loaders {task_name:loader}, out_dim {task_name:num_classes}
-    '''
-    assert train_dataset.number_classes == val_dataset.number_classes, 'Train/Val has different number of classes'
-    num_classes = train_dataset.number_classes
-
-    # Calculate the boundary index of classes for splits
-    # Ex: [0,2,4,6,8,10] or [0,50,60,70,80,90,100]
-    split_boundaries = [0, first_split_sz]
-    while split_boundaries[-1] < num_classes:
-        split_boundaries.append(split_boundaries[-1] + other_split_sz)
-    print('split_boundaries:', split_boundaries)
-    assert split_boundaries[-1] == num_classes, 'Invalid split size'
-
-    # Assign classes to each splits
-    # Create the dict: {split_name1:[2,6,7], split_name2:[0,3,9], ...}
-    class_lists = {i: list(range(split_boundaries[i], split_boundaries[i + 1])) for i in
-                       range(len(split_boundaries) - 1)}
-
-    print(class_lists)
-
-    # Generate the dicts of splits
-    # Ex: {split_name1:dataset_split1, split_name2:dataset_split2, ...}
-    train_dataset_splits = {}
-    val_dataset_splits = {}
-    task_output_space = {}
-
-    if random_split:
-        batch_indices = (torch.rand(len(train_dataset)) * len(split_boundaries)).long()
-        batch_indices = batch_indices.long()
-        dataset_len = len(train_dataset)
-        train_set_len = int(dataset_len * 0.8)
-        train_indices = batch_indices[:train_set_len]
-        val_indices = batch_indices[train_set_len:]
-
-        for name, _ in class_lists.items():
-            train_subset = Subset(train_dataset, torch.where(train_indices == name)[0])
-            # train_subset.labels = train_class_indices[train_indices == name]  # torch.ones(len(train_subset), 1) * name
-            # train_subset.attr = train_subset.labels
-            train_subset.class_list = range(train_dataset.number_classes)
-
-            val_subset = Subset(train_dataset, train_set_len + torch.where(val_indices == name)[0])
-            # val_subset.labels = val_class_indices[val_indices == name]  # torch.ones(len(val_subset), 1) * name
-            val_subset.class_list = range(train_dataset.number_classes)
-            # val_subset.attr = val_subset.labels
-
-            train_dataset_splits[name] = AppendName(train_subset, name)
-            val_dataset_splits[name] = AppendName(val_subset, name)
-            task_output_space[name] = (batch_indices == name).sum()
-    else:
-        for name, class_list in class_lists.items():
-            train_dataset_splits[name] = AppendName(Subclass(train_dataset, class_list, remap_class), name)
-            val_dataset_splits[name] = AppendName(Subclass(val_dataset, class_list, remap_class), name)
-            task_output_space[name] = len(class_list)
-
-    return train_dataset_splits, val_dataset_splits, task_output_space
-
-
-def data_split(dataset, dataset_name, num_batches=5, num_classes=10, random_split=False,
+def data_split(dataset, dataset_name, return_classes=False, return_task_as_class=False, num_batches=5, num_classes=10, random_split=False,
                limit_data=None, dirichlet_split_alpha=None, dirichlet_equal_split=True, reverse=False,
                limit_classes=-1):
     if limit_classes > 0:
@@ -225,8 +159,8 @@ def data_split(dataset, dataset_name, num_batches=5, num_classes=10, random_spli
         val_subset.class_list = batch_split[name]
         # val_subset.attr = val_subset.labels
 
-        train_dataset_splits[name] = AppendName(train_subset, name)
-        val_dataset_splits[name] = AppendName(val_subset, name)
+        train_dataset_splits[name] = AppendName(train_subset, name, return_classes=return_classes, return_task_as_class=return_task_as_class)
+        val_dataset_splits[name] = AppendName(val_subset, name, return_classes=return_classes, return_task_as_class=return_task_as_class)
         task_output_space[name] = (batch_indices[:, name] == 1).sum()
     if dirichlet_split_alpha != None:
         print("Created dataset with class split:")
@@ -241,20 +175,3 @@ def data_split(dataset, dataset_name, num_batches=5, num_classes=10, random_spli
         f"Validation dataset with splits: {[(idx, len(data)) for idx, data in enumerate(val_dataset_splits.values())]}")
 
     return train_dataset_splits, val_dataset_splits, task_output_space
-
-
-def PermutedGen(train_dataset, val_dataset, n_permute, remap_class=False):
-    sample, _ = train_dataset[0]
-    n = sample.numel()
-    train_datasets = {}
-    val_datasets = {}
-    task_output_space = {}
-    for i in range(1, n_permute + 1):
-        rand_ind = list(range(n))
-        shuffle(rand_ind)
-        name = str(i)
-        first_class_ind = (i - 1) * train_dataset.number_classes if remap_class else 0
-        train_datasets[name] = AppendName(Permutation(train_dataset, rand_ind), name, first_class_ind=first_class_ind)
-        val_datasets[name] = AppendName(Permutation(val_dataset, rand_ind), name, first_class_ind=first_class_ind)
-        task_output_space[name] = train_dataset.number_classes
-    return train_datasets, val_datasets, task_output_space

dataloaders/wrapper.py
@@ -3,6 +3,7 @@ from copy import deepcopy
 
 import torch
 import torch.utils.data as data
+import numpy as np
 
 
 class CacheClassLabel(data.Dataset):
@@ -45,19 +46,27 @@ class AppendName(data.Dataset):
     """
     A dataset wrapper that also return the name of the dataset/task
     """
-    def __init__(self, dataset, name, first_class_ind=0):
+    def __init__(self, dataset, name, return_classes=False, return_task_as_class=False, task_id=0, first_class_ind=0):
         super(AppendName,self).__init__()
         self.dataset = dataset
         self.name = name
-        self.first_class_ind = first_class_ind  # For remapping the class index
+        self.first_class_ind = first_class_ind
+        self.task_id = task_id # For remapping the class index
+        self.return_classes = return_classes
+        self.return_task_as_class = return_task_as_class
 
     def __len__(self):
         return len(self.dataset)
 
     def __getitem__(self, index):
-        img,target = self.dataset[index]
+        img, target = self.dataset[index]
         target = target + self.first_class_ind
-        return img, target #, self.name
+        out_dict = {}
+        if self.return_task_as_class:
+            out_dict["y"] = np.array(self.task_id, dtype=np.int64)
+        elif self.return_classes:
+            out_dict["y"] = np.array(target, dtype=np.int64)
+        return img, out_dict #target #, self.name
 
 
 class Subclass(data.Dataset):

guided_diffusion/dist_util.py
@@ -22,7 +22,7 @@ def setup_dist(args):
     """
     Setup a distributed process group.
     """
-    if args.gpu_id:
+    if args.gpu_id!=-2:
         os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
         os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpu_id)
 
@@ -51,7 +51,7 @@ def dev():
     Get the device to use for torch.distributed.
     """
     if th.cuda.is_available():
-        return th.device(f"cuda:0")
+        return th.device(f"cuda")
     return th.device("cpu")
 
 

scripts/image_train.py
@@ -21,21 +21,29 @@ from guided_diffusion.train_util import TrainLoop
 import os
 import wandb
 
+import torch
+
+print("Is CUDA available:", torch.cuda.is_available())
+
+
 # os.environ["WANDB_MODE"] = "disabled"
 
 def yielder(loader):
     while True:
         yield from loader
 
+
 def main():
     args = create_argparser().parse_args()
-    wandb.init(project="continual_diffusion", name=args.experiment_name, config=args)
-    os.environ["OPENAI_LOGDIR"]= f"results/{args.experiment_name}"
+    os.environ["WANDB_API_KEY"] = args.wandb_api_key
+    wandb.init(project="continual_diffusion", name=args.experiment_name, config=args, entity="generative_cl")
+    os.environ["OPENAI_LOGDIR"] = f"results/{args.experiment_name}"
 
     dist_util.setup_dist(args)
     logger.configure()
 
-    train_dataset, val_dataset, image_size, image_channels = base.__dict__[args.dataset](args.dataroot, args.train_aug)
+    train_dataset, val_dataset, image_size, image_channels = base.__dict__[args.dataset](args.dataroot,
+                                                                                         train_aug=args.train_aug)
 
     args.image_size = image_size
     args.in_channels = image_channels
@@ -56,6 +64,8 @@ def main():
     logger.log("creating data loader...")
 
     train_dataset_splits, val_dataset_splits, task_output_space = data_split(dataset=train_dataset,
+                                                                             return_classes=args.class_cond,
+                                                                             return_task_as_class=args.use_task_index,
                                                                              dataset_name=args.dataset.lower(),
                                                                              num_batches=args.num_tasks,
                                                                              num_classes=n_classes,
@@ -70,12 +80,21 @@ def main():
                                                batch_size=args.batch_size, shuffle=True,
                                                drop_last=True)
 
-
+        if args.class_cond:
+            if args.use_task_index:
+                max_class = task_id
+            else:
+                raise NotImplementedError() #Classes seen so far for plotting and sampling
+        else:
+            max_class = None
         logger.log("training...")
+        num_steps = args.num_steps
+        if task_id == 0:
+            num_steps = num_steps
         TrainLoop(
             model=model,
             diffusion=diffusion,
-            task_id = task_id,
+            task_id=task_id,
             data=yielder(train_dataset_loader),
             batch_size=args.batch_size,
             microbatch=args.microbatch,
@@ -91,14 +110,17 @@ def main():
             schedule_sampler=schedule_sampler,
             weight_decay=args.weight_decay,
             lr_anneal_steps=args.lr_anneal_steps,
-            num_steps=args.num_steps,
+            num_steps=num_steps,
             image_size=args.image_size,
-            in_channels=args.in_channels
+            in_channels=args.in_channels,
+            class_cond=args.class_cond,
+            max_class=max_class
         ).run_loop()
 
 
 def create_argparser():
     defaults = dict(
+        wandb_api_key="",
         experiment_name="test",
         dataroot="data/",
         dataset="MNIST",
@@ -107,7 +129,7 @@ def create_argparser():
         weight_decay=0.0,
         lr_anneal_steps=0,
         batch_size=32,
-        microbatch =-1,  # -1 disables microbatches
+        microbatch=-1,  # -1 disables microbatches
         ema_rate="0.9999",  # comma-separated list of EMA values
         log_interval=10,
         save_interval=5000,
@@ -124,7 +146,8 @@ def create_argparser():
         train_aug=False,
         limit_data=None,
         num_steps=10000,
-        scheduler_rate=1.0
+        scheduler_rate=1.0,
+        use_task_index=True
     )
     defaults.update(model_and_diffusion_defaults())
     parser = argparse.ArgumentParser()
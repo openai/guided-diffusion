Fix validation for bigger number of samples and nll calculation

evaluations/validation.py
@@ -67,15 +67,17 @@ class Validator:
                 if dataset.lower() in ["fashionmnist", "doublemnist"]:
                     x = x.repeat([1, 3, 1, 1])
                 distribution_orig.append(self.score_model_func(x.to(self.score_model_device)).cpu().detach().numpy())
-        example, _ = train_loop.generate_examples(task_id=task_id, n_examples_per_task=n_generated_examples, only_one_task=True,
-                                                  batch_size=batch_size)
-        example = example.to(self.score_model_device)
-        if dataset.lower() in ["fashionmnist", "doublemnist"]:
-            example = example.repeat([1, 3, 1, 1])
-        distribution_gen = self.score_model_func(example).cpu().numpy().reshape(-1, self.dims)
-        # distribution_gen.append(self.score_model_func(example))
 
-        # distribution_gen = torch.cat(distribution_gen).cpu().detach().numpy().reshape(-1, self.dims)
+        examples_to_generate = n_generated_examples
+        while examples_to_generate > 0:
+            example, _ = train_loop.generate_examples(task_id=task_id, n_examples_per_task=batch_size, only_one_task=True)
+            example = example.to(self.score_model_device)
+            if dataset.lower() in ["fashionmnist", "doublemnist"]:
+                example = example.repeat([1, 3, 1, 1])
+            distribution_gen.append(self.score_model_func(example).cpu().detach())#.numpy().reshape(-1, self.dims))
+            # distribution_gen = self.score_model_func(example).cpu().numpy().reshape(-1, self.dims)
+
+        distribution_gen = torch.cat(distribution_gen).numpy().reshape(-1, self.dims)
         # distribution_gen = np.array(np.concatenate(distribution_gen)).reshape(-1, self.dims)
         if not precalculated_statistics:
             distribution_orig = np.array(np.concatenate(distribution_orig)).reshape(-1, self.dims)

guided_diffusion/gaussian_diffusion.py
@@ -26,11 +26,16 @@ def get_named_beta_schedule(schedule_name, num_diffusion_timesteps, first_step_b
     Beta schedules may be added, but should not be removed or changed once
     they are committed to maintain backwards compatibility.
     """
+    if num_diffusion_timesteps < 10:
+        scale = 100 / num_diffusion_timesteps
+        multiply = 0.01
+    else:
+        scale = 1000 / num_diffusion_timesteps
+        multiply = 0.001
     if schedule_name == "linear":
         # Linear schedule from Ho et al, extended to work for any number of
         # diffusion steps.
-        scale = 1000 / num_diffusion_timesteps
-        beta_start = scale * 0.0001
+        beta_start = scale * multiply
         beta_end = scale * 0.02
         return np.linspace(
             beta_start, beta_end, num_diffusion_timesteps, dtype=np.float64
@@ -43,8 +48,7 @@ def get_named_beta_schedule(schedule_name, num_diffusion_timesteps, first_step_b
     elif schedule_name == "linear_combine":
         assert first_step_beta is not None
         first_step_beta = float(first_step_beta)
-        scale = 1000 / num_diffusion_timesteps
-        beta_start = scale * 0.0001
+        beta_start = scale * multiply
         beta_end = scale * 0.02
         linear_betas = np.linspace(
             beta_start, beta_end, num_diffusion_timesteps, dtype=np.float64
@@ -742,17 +746,19 @@ class GaussianDiffusion:
         )
         kl = normal_kl(
             true_mean, true_log_variance_clipped, out["mean"], out["log_variance"]
-        ) # Calculate kl for all examples even with timestep=0 It will be overwritten later on
+        )  # Calculate kl for all examples even with timestep=0 It will be overwritten later on
         kl = mean_flat(kl) / np.log(2.0)
         if self.dae_model:
+            # out["log_variance"] = th.zeros_like(out["log_variance"]) + self.constant_sigma * 2
             decoder_mse = (out["mean"] - x_start) ** 2
             decoder_loss_0 = mean_flat(decoder_mse)
         else:
             decoder_nll = -discretized_gaussian_log_likelihood(
                 x_start, means=out["mean"], log_scales=0.5 * out["log_variance"]
             )
+            # print(th.exp(-0.5 * out["log_variance"][t==0]).mean().item())
             assert decoder_nll.shape == x_start.shape
-            decoder_loss_0 = mean_flat(decoder_nll)/ np.log(2.0)
+            decoder_loss_0 = mean_flat(decoder_nll) / np.log(2.0)
 
         # At the first timestep return the decoder NLL or MSE,
         # otherwise return KL(q(x_{t-1}|x_t,x_0) || p(x_{t-1}|x_t))
@@ -873,7 +879,10 @@ class GaussianDiffusion:
                 model_kwargs=model_kwargs,
             )["output"]
             if self.loss_type == LossType.RESCALED_KL:
-                terms["loss"] *= self.num_timesteps
+                if self.dae_model:
+                    terms["loss"] *= (self.num_timesteps - 1)
+                else:
+                    terms["loss"] *= self.num_timesteps
         elif self.loss_type == LossType.MSE or self.loss_type == LossType.RESCALED_MSE:
             model_output = model(x_t, self._scale_timesteps(t), **model_kwargs)
 

scripts/image_nll.py
@@ -7,7 +7,9 @@ import os
 
 import numpy as np
 import torch.distributed as dist
+from dataloaders import base
 
+from dataloaders.datasetGen import data_split
 from guided_diffusion import dist_util, logger
 from guided_diffusion.image_datasets import load_data
 from guided_diffusion.script_util import (
@@ -16,35 +18,83 @@ from guided_diffusion.script_util import (
     add_dict_to_argparser,
     args_to_dict,
 )
+import torch
+# from torch.utils import data
+# from validation import Validator
 
+def yielder(loader):
+    while True:
+        yield from loader
 
 def main():
     args = create_argparser().parse_args()
+    dist_util.setup_dist(args)
+    os.environ["OPENAI_LOGDIR"] = f"results/{args.experiment_name}"
 
-    dist_util.setup_dist()
+    if args.seed:
+        print("Using manual seed = {}".format(args.seed))
+        torch.manual_seed(args.seed)
+        torch.cuda.manual_seed(args.seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+    else:
+        print("WARNING: Not using manual seed - your experiments will not be reproducible")
+
+    # os.environ["WANDB_API_KEY"] = args.wandb_api_key
+    # wandb.init(project="continual_diffusion", name=args.experiment_name, config=args, entity="generative_cl")
+    # os.environ["OPENAI_LOGDIR"] = f"results/{args.experiment_name}"
+
     logger.configure()
 
+    train_dataset, val_dataset, image_size, image_channels = base.__dict__[args.dataset](args.dataroot,
+                                                                                         train_aug=args.train_aug)
+
+    args.image_size = image_size
+    args.in_channels = image_channels
+    if args.dataset.lower() == "celeba":
+        n_classes = 10
+    else:
+        n_classes = train_dataset.number_classes
+
+    args.num_classes = args.num_tasks  # n_classes
     logger.log("creating model and diffusion...")
     model, diffusion = create_model_and_diffusion(
         **args_to_dict(args, model_and_diffusion_defaults().keys())
     )
+    model_path = f"results/{args.experiment_name}/{args.model_path}"
     model.load_state_dict(
-        dist_util.load_state_dict(args.model_path, map_location="cpu")
+        dist_util.load_state_dict(model_path, map_location="cpu")
     )
+    # if not os.environ.get("WANDB_MODE") == "disabled":
+    #     wandb.watch(model, log_freq=10)
     model.to(dist_util.dev())
-    model.eval()
 
     logger.log("creating data loader...")
-    data = load_data(
-        data_dir=args.data_dir,
-        batch_size=args.batch_size,
-        image_size=args.image_size,
-        class_cond=args.class_cond,
-        deterministic=True,
-    )
+
+    train_dataset_splits, val_dataset_splits, task_output_space = data_split(dataset=train_dataset,
+                                                                             return_classes=args.class_cond,
+                                                                             return_task_as_class=args.use_task_index,
+                                                                             dataset_name=args.dataset.lower(),
+                                                                             num_batches=args.num_tasks,
+                                                                             num_classes=n_classes,
+                                                                             random_split=args.random_split,
+                                                                             limit_data=args.limit_data,
+                                                                             dirichlet_split_alpha=args.dirichlet,
+                                                                             reverse=args.reverse,
+                                                                             limit_classes=args.limit_classes)
+
+    val_loaders = []
+    for task_id in range(args.num_tasks):
+        val_data = val_dataset_splits[task_id]
+        val_loader = torch.utils.data.DataLoader(dataset=val_data, batch_size=args.batch_size, shuffle=False)
+        val_loaders.append(val_loader)
+
 
     logger.log("evaluating...")
-    run_bpd_evaluation(model, diffusion, data, args.num_samples, args.clip_denoised)
+    diffusion.dae_model = False
+    for task_id in range(args.num_tasks):
+        data = yielder(val_loaders[task_id])
+        run_bpd_evaluation(model, diffusion, data, args.num_samples, args.clip_denoised)
 
 
 def run_bpd_evaluation(model, diffusion, data, num_samples, clip_denoised):
@@ -84,7 +134,25 @@ def run_bpd_evaluation(model, diffusion, data, num_samples, clip_denoised):
 
 def create_argparser():
     defaults = dict(
-        data_dir="", clip_denoised=True, num_samples=1000, batch_size=1, model_path=""
+        seed=13,
+        wandb_api_key="",
+        experiment_name="test",
+        dataroot="data/",
+        dataset="MNIST",
+        data_dir="",
+        clip_denoised=True,
+        num_samples=1000,
+        batch_size=1,
+        model_path="",
+        gpu_id=-1,
+        reverse=False,
+        dirichlet=None,
+        num_tasks=5,
+        limit_classes=-1,
+        random_split=False,
+        train_aug=False,
+        limit_data=None,
+        use_task_index=True,
     )
     defaults.update(model_and_diffusion_defaults())
     parser = argparse.ArgumentParser()